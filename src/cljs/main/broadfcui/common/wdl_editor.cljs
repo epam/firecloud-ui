743b32167a3c1f1bc9aa59e9426e482ebf887c54
Timofey Alyakin <Timofey_Alyakin@epam.com>
2017-10-10 17:44:30 +0300

    Added functionality for Script building button

--- src/cljs/main/broadfcui/common/wdl_editor.cljs
+++ src/cljs/main/broadfcui/common/wdl_editor.cljs
@@ -6,17 +6,24 @@
     [broadfcui.common.pipeline :refer [PipelineBuilder]]))
 
 (react/defc WDLEditor
-  {:render
-   (fn [{:keys [props]}]
-     [:div {:className "wdl-editor"}
-      [:div {:className "wdl-title noselect"}
-       [WDLEditorButtons {:onWDLChange (:onWDLChange props)}]
-       [WDLEditorErrorsPanel {:wasRunned true :errors #js []  :isBuilding false}]
-       ]
-      [:div {:className "textarea-wrapper"}
-       [CodeMirror {:text (:WDL props) :read-only? (:read-only? props)}]]])
-   :refresh
-   (constantly nil)})
+  {:get-initial-state (fn [{:keys [props]}] {:currentWDLState (:WDL props)})
+  :render
+    (fn [{:keys [props state]}]
+      (let [{:keys [currentWDLState]} @state]
+        [:div {:className "wdl-editor"}
+          [:div {:className "wdl-title noselect"}
+            [WDLEditorButtons {:onWDLBuildClick (fn [] ((:onWDLChange props) (:currentWDLState @state)))}]
+            [WDLEditorErrorsPanel {:wasRunned true :errors #js []  :isBuilding false}]
+          ]
+          [:div {:className "textarea-wrapper"}
+            [CodeMirror {:text       currentWDLState
+              :read-only? (:read-only? props)
+              :initialize (fn [self]
+                (self :add-listener "change"
+                      (fn [] (swap! state assoc :currentWDLState (js->clj (self :call-method "getValue"))))))
+              }]]]))
+             :refresh
+    (constantly nil)})
 
 (react/defc- EditorButton
   {:render
@@ -35,41 +42,7 @@
       [EditorButton {:icon-name "download" :button-text "Download"
                      :onClickCallback (fn [e] ())}]         ;todo click handler
       [EditorButton {:icon-name "play" :button-text "Build"
-                     :onClickCallback (fn [e]
-                                        (def newWDL "workflow SimpleVariantSelection {
-  File gatk
-  File refFasta
-  File refIndex
-  File refDict
-  String name
-
-  call haplotypeCaller {
-    input:
-      GATK = gatk,
-      RefFasta = refFasta,
-      RefIndex = refIndex,
-      RefDict = refDict,
-      sampleName = name,
-  }
-}
-
-task haplotypeCaller {
-  File GATK
-  File RefFasta
-  File RefIndex
-  File RefDict
-  String sampleName
-  File inputBAM
-  File bamIndex
-
-
-  output {
-    File rawVCF = \"${sampleName}.raw.indels.snps.vcf\"
-  }
-}
-" )
-                                                ((:onWDLChange props) newWDL)
-                                                )}]         ;todo click handler
+                     :onClickCallback (fn [e] ((:onWDLBuildClick props)))}]
       ])})
 
 (react/defc WDLEditorErrorsPanel
